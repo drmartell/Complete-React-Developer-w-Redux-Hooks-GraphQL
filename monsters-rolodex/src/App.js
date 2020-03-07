674d299ca0248c4ecc0dcfb102a9790520a0d7e8
drmartell <drmartell@gmail.com>
2020-03-06 17:17:27 -0800

    componentDidMount

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state =  {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 1
-        },
-        {
-          name: 'Dracula',
-          id: 2
-        },
-        {
-          name: 'Zombie',
-          id: 3
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
   render() {
