c7c0eb84f5fa68954759608137f2c943e8acd573
drmartell <drmartell@gmail.com>
2020-03-08 11:16:05 -0700

    CardList component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import './components/CardList';
+import { CardList } from './components/CardList';
 
 class App extends React.Component {
   constructor() {
@@ -18,10 +20,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <CardList name='Casey'>
         {
           this.state.monsters.map(monster =>
           <h1 key={monster.id}>{monster.name}</h1>)
         }
+        </CardList>
       </div>
     );
   }
